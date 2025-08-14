5e93de2706f699ee7e34a9aaa63c64b8687d3fa0
PaReng <korreppsj@naver.com>
2025-08-15 02:07:57 +0900

    [Feat] 플레이어 점프 기능 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [Header("player stat")]
     public int playerMoveSpeed = 5;
+    public int playerJumpForce = 5;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
@@ -16,7 +30,30 @@ public class NewBehaviourScript : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float MoveY = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = new Vector3(moveX, 0, MoveY).normalized;
+        Vector3 moveDir = new Vector3(moveX, 0, MoveY);
         transform.Translate(moveDir * playerMoveSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector3.up * playerJumpForce, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+        
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }

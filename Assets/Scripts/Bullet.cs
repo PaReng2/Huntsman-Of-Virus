0b8a9a9cd09a7a7393eeca9d5abffed4bcb12e8c
hyosung0919 <topruna0919@naver.com>
2025-08-16 17:21:28 +0900

    [Fix]총알을 원하는 방향으로 쏠수 있게, 적을 맞추면 적이 없어지게

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float DestroyDelay = 5.0f;
+    public float speed = 20f;
+    public float DestroyDelay = 5f;
+
+    private Vector3 moveDir;
+
     void Start()
     {
         Destroy(gameObject, DestroyDelay);
     }
+
     void Update()
     {
-        transform.Translate(Vector3.forward * 1f);
+        transform.position += moveDir * speed * Time.deltaTime;
     }
 
+    public void SetDirection(Vector3 dir)
+    {
+        moveDir = dir;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+
+        }
+    }
 }
--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public GameObject Bullet;
-    public Transform FirePos;
+    public GameObject BulletPrefab;   // ÃÑ¾Ë ÇÁ¸®ÆÕ
+    public Transform FirePos;         // ¹ß»ç À§Ä¡
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(Bullet, FirePos.transform.position, FirePos.transform.rotation);
+            Shoot();
         }
+    }
+
+    void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(1000);
+        }
+
+        Vector3 dir = (targetPoint - FirePos.position).normalized;
+
+        GameObject bullet = Instantiate(BulletPrefab, FirePos.position, Quaternion.identity);
+
+        bullet.GetComponent<Bullet>().SetDirection(dir);
     }
 }

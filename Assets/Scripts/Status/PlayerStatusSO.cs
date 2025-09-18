0cfa81d5f30ea49a85977f4c82d1adcd350c733f
PaReng2 <korreppsj@naver.com>
2025-09-18 20:03:31 +0900

    [Feat] 플레이어 스탯 스크립터블 오브젝트 추가

---
+++ Assets/Scripts/Status/PlayerStatusSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName ="PlayerStat", menuName ="Player/PlayerStat")]
+public class PlayerStatusSO : ScriptableObject
+{
+    public float playerHP;
+    public float playerOffensivePower;
+    public float playerAttackDelay;
+    public float playerMoveSpeed;
+    public float playerAttackRange;
+}

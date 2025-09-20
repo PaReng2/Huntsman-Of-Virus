5a96f88cd820f87142b395e0dc2b3e6eac1d2d8f
PaReng <korreppsj@naver.com>
2025-09-21 00:45:54 +0900

    [Feat] 파일 정리, 튜토리얼용 적 스크립트 추가

--- Assets/Scripts/PlayerScripts/AttackRangeDealer/PlayerAttackRangeDealer.cs
+++ Assets/Scripts/PlayerScripts/AttackRangeDealer/PlayerAttackRangeDealer.cs
@@ -30,24 +30,23 @@ public class PlayerAttackRangeDealer : MonoBehaviour
 
     void Attack()
     {
-        // 1. Raycast를 통해 타겟 지점을 찾습니다.
-        // 이 부분은 총알이 향할 방향을 결정하는 데 여전히 유용합니다.
+        //raycast를 통해 타겟 지점을 찾기
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
         Vector3 targetPoint;
 
-        // 2. Raycast가 특정 레이어에 충돌했는지 확인합니다.
-        // 충돌했다면, 그 지점을 총알의 목표로 설정합니다.
+        //Raycast가 특정 레이어에 충돌했는지 확인
+        // 충돌했다면, 그 지점을 총알의 목표로 설정
         if (Physics.Raycast(ray, out hit, float.MaxValue, shootableMask))
         {
             targetPoint = hit.point;
         }
         else
         {
-            // 3. 충돌하지 않았다면, 카메라에서 일정 거리 떨어진 가상의 지점을 목표로 설정합니다.
-            // 이렇게 하면 어떤 오브젝트와도 충돌하지 않아도 총알이 발사됩니다.
-            targetPoint = ray.GetPoint(100f); // 100f는 원하는 거리로 조절 가능합니다.
+            //충돌하지 않았다면, 카메라에서 일정 거리 떨어진 가상의 지점을 목표로 설정
+            //어떤 오브젝트와도 충돌하지 않아도 총알이 발사
+            targetPoint = ray.GetPoint(100f); // 100f는 원하는 거리로 조절 가능
         }
 
         // 4. Raycast 결과(실제 충돌 지점 또는 가상의 지점)를 기반으로 총알을 발사합니다.

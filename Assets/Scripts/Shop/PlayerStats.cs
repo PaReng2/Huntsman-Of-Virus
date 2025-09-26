1935636fa90f2f32f0ee9e10748a1148c42043ff
AntiJ0 <firefly17569@gmail.com>
2025-09-27 00:20:22 +0900

    [Feat] 상점 구매 불가능 오류 수정, 상점 내 골드 보유량 표시 텍스트 추가

--- Assets/Scripts/Shop/PlayerStats.cs
+++ Assets/Scripts/Shop/PlayerStats.cs
@@ -1,32 +1,55 @@
 using UnityEngine;
+using TMPro;
 
 // 플레이어의 기본적인 스탯 (추가 및 수정 가능)
 
+
 public class PlayerStats : MonoBehaviour
 {
-    public int gold = 100;
-    public int hp = 80;
+    public int gold = 0;
+    public int hp = 100;
     public int maxHp = 100;
     public int attack = 10;
 
+    [Header("UI")]
+    public TMP_Text goldText; 
 
-    public void AddGold(int amount) => gold += amount;
+    private void Start()
+    {
+        UpdateGoldUI();
+    }
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        UpdateGoldUI();
+    }
+
     public bool SpendGold(int amount)
     {
         if (gold >= amount)
         {
             gold -= amount;
+            UpdateGoldUI();
             return true;
         }
         return false;
     }
 
+    private void UpdateGoldUI()
+    {
+        if (goldText != null)
+        {
+            goldText.text = "Gold : " + gold;
+        }
+    }
 
     public void Heal(int amount)
     {
-        hp = Mathf.Min(maxHp, hp + amount);
+        hp += amount;
+        if (hp > maxHp)
+            hp = maxHp;
     }
-
 
     public void AddAttack(int amount)
     {

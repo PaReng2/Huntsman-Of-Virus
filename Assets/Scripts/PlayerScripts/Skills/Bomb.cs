046c1db70588f01e5b8c6e135cf7d10e71c32a30
hyosung0919 <topruna0919@naver.com>
2025-12-10 01:30:51 +0900

    [Fix]폭발 버그 수정

--- Assets/Scripts/PlayerScripts/Skills/Bomb.cs
+++ Assets/Scripts/PlayerScripts/Skills/Bomb.cs
@@ -8,6 +8,7 @@ public class Bomb : MonoBehaviour
     public int Damage;
 
     public float explosionDelay = 1.0f;
+    public float explosionRadius = 5f;
 
     private bool hasExploded = false;
 
@@ -52,16 +53,40 @@ public class Bomb : MonoBehaviour
         if (hasExploded) return;
         hasExploded = true;
 
+        PerformExplosionDamage();
+
         Collider col = GetComponent<Collider>();
         if (col != null)
         {
             col.enabled = false;
         }
 
-
         CancelInvoke("ExplodeAndDestroy");
 
         Destroy(gameObject, explosionDelay);
+    }
+
+    private void PerformExplosionDamage()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("Enemy"))
+            {
+                ChaseEnemy enemy = hitCollider.GetComponent<ChaseEnemy>();
+                if (enemy != null)
+                {
+                    enemy.TakeDamage(Damage);
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawSphere(transform.position, explosionRadius);
 
     }
 }
